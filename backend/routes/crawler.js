080bbc060f0572cb5c21a60e1dea3c3a8489457a
Sai Lokesh <sailokeshchirumarri@gmail.com>
2024-12-27 20:50:56 +0530

    defualt websites added

--- backend/routes/crawler.js
+++ backend/routes/crawler.js
@@ -3,5 +3,8 @@ const { crawlEcommerceSites } = require('../controllers/crawlerController');
 const router = express.Router();
 
 router.post('/crawl', crawlEcommerceSites);
+router.get("/", (req, res) => {
+    res.send({success: "true"})
+})
 
 module.exports = router;
