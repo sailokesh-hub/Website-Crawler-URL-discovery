5021350ffbd6c3ecf8dcff3889fbf5815d371c22
Sai Lokesh <sailokeshchirumarri@gmail.com>
2024-12-27 23:58:13 +0530

    styling

--- backend/routes/crawler.js
+++ backend/routes/crawler.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/crawl', crawlEcommerceSites);
 
 router.get("/", function(req, res) {
+    console.log("request recieved")
     res.send("true");
   });
 
