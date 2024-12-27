b0547016720afbe44a403aa921c2c749ae0c9e02
Sai Lokesh <sailokeshchirumarri@gmail.com>
2024-12-27 16:15:04 +0530

    13 commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,21 @@ const App = () => {
     const [domains, setDomains] = useState('');
     const [results, setResults] = useState(null);
 
+    const USER_AGENTS = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+    ];
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const { data } = await axios.post('https://website-crawler-url-discovery-1.onrender.com/api/crawler/crawl', {
                 domains: domains.split('\n'),
+            },{
+                headers: {
+                    'User-Agent': USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)],
+                },
             });
             setResults(data);
         } catch (error) {
