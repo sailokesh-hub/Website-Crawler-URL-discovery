e838dc0a3f93b9b0eeb8c874b7f959e50d279521
Sai Lokesh <sailokeshchirumarri@gmail.com>
2024-12-27 16:08:01 +0530

    12 commit

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ const app = express();
 require('dotenv').config()
 const crawlerRoutes = require('./routes/crawler');
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: 'https://website-crawler-url-discovery.onrender.com' // Replace with your frontend's origin
+}))
 app.use('/api/crawler', crawlerRoutes);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
